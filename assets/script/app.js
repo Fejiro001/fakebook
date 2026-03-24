d1963008f30f867d1aa65f874ce67da859937607
Fejiro001 <aberefejiro@gmail.com>
2026-03-24 13:27:11 -0500

    Added event listener for file selection

--- assets/script/app.js
+++ assets/script/app.js
@@ -1,8 +1,17 @@
 "use strict";
 
-const postsForm = document.getElementById("posts-form");
+const postForm = document.getElementById("posts-form");
 const postText = document.getElementById("post");
 const postImage = document.getElementById("file");
-const imageFileName = document.getElementById("file-name");
+const imageFileName = document.querySelector(".file-name");
 const postContainer = document.getElementById("posts-section");
 
+let fileName = "";
+
+postImage.addEventListener("change", () => {
+  fileName = postImage.files[0]?.name;
+
+  if (fileName) {
+    imageFileName.textContent = fileName;
+  }
+});
