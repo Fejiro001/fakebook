e543d5ea43fedbfdec10a6f7683020fa4b87ab12
Fejiro001 <aberefejiro@gmail.com>
2026-03-24 11:50:16 -0500

    Extended User parent class and initialized constructor method

--- assets/script/Subscriber.js
+++ assets/script/Subscriber.js
@@ -1,1 +1,15 @@
 "use strict";
+import { User } from "./User.js";
+
+export class Subscriber extends User {
+  #pages;
+  #groups;
+  #canMonetize;
+
+  constructor(id, fullName, userName, email, pages, groups, canMonetize) {
+    super(id, fullName, userName, email);
+    this.pages = pages;
+    this.groups = groups;
+    this.canMonetize = canMonetize;
+  }
+}
