d2268c5b2b0e610a136194f7ee321226021cacd6
Fejiro001 <aberefejiro@gmail.com>
2026-03-24 12:56:18 -0500

    Selected elements for form submission and post display

--- assets/script/app.js
+++ assets/script/app.js
@@ -1,1 +1,8 @@
 "use strict";
+
+const postsForm = document.getElementById("posts-form");
+const postText = document.getElementById("post");
+const postImage = document.getElementById("file");
+const imageFileName = document.getElementById("file-name");
+const postContainer = document.getElementById("posts-section");
+
